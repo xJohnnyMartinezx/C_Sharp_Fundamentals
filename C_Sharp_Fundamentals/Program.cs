4d6029acc2c11ed93d093f1ae4de7e87854b4108
xJohnnyMartinezx <mrtnz.jn@gmail.com>
2024-03-29 11:52:57 -0500

    retreived user input an dprinted it.

--- C_Sharp_Fundamentals/Program.cs
+++ C_Sharp_Fundamentals/Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+//console.write line will output a line to the console.
 Console.WriteLine("Hello, World!");
 Console.WriteLine("My name is Johnny");
+Console.WriteLine("Please enter your name: ");
+
+//Console.readline() will get what user inputs into the console.
+string name = Console.ReadLine();
+
+Console.WriteLine("Hello, " + name);
